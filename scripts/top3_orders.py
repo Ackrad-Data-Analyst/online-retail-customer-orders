00d43f774ef8ec2e9e48dea34807cbdabc04cb32
Ackrad-Data-Analyst <sethackrad@gmail.com>
2025-12-01 00:07:07 -0800

    Update top3_orders.py

--- scripts/top3_orders.py
+++ scripts/top3_orders.py
@@ -1,5 +1,5 @@
 import pandas as pd
 orders = pd.read_csv('data/Order.csv')
 top3 = orders.sort_values('TotalProfit', ascending=False).head(3)
-print('Top 3 Most Profitable Orders:')
+print('Top 5 Most Profitable Orders:')
 print(top3[['OrderID','CustomerID','TotalProfit','OrderDate']].to_string(index=False))
